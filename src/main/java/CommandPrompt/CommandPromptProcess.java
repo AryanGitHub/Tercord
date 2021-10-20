bbb9a0505b98c42b7faceab4b60ecb925fabaf0f
Aryan Pandey <aryanandrod26@gmail.com>
2021-10-20 14:07:02 +0530

    added redirectErrorStream to true in CommandPromptProcess to display errors too along with output in discord message.

--- src/main/java/CommandPrompt/CommandPromptProcess.java
+++ src/main/java/CommandPrompt/CommandPromptProcess.java
@@ -35,6 +35,7 @@ public class CommandPromptProcess {
 	public void deployCommandPrompt () throws IOException {
 		if (p == null || !p.isAlive() || processState == ProcessState.initialized) {
 		processState = ProcessState.deploying; //not much use cus its set to idle after deployed
+		pb.redirectErrorStream(true);//now we will also see errors while reading the output
 		p = pb.start();
 		readingCommandPrompt = new ReadingCommandPrompt(p.getInputStream());
 		readingCommandPrompt.start();
